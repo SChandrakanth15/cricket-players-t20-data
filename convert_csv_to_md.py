400f3a96e49b1af2dbb67336d30dab0515ef1c9e
SChandrakanth15 <schandrakanth15@gmail.com>
2024-12-30 15:57:01 +0530

    changed the workflow file to display in the console

--- convert_csv_to_md.py
+++ convert_csv_to_md.py
@@ -2,7 +2,6 @@ import csv
 
 # Read the CSV file
 input_file = 't20.csv'
-output_file = 'cricket_players.md'
 
 with open(input_file, 'r') as csv_file:
     reader = csv.reader(csv_file)
@@ -15,8 +14,7 @@ with open(input_file, 'r') as csv_file:
     for row in reader:
         markdown_table += "| " + " | ".join(row) + " |\n"
 
-# Write the Markdown table to a file
-with open(output_file, 'w') as md_file:
-    md_file.write(markdown_table)
-
-print(f"Markdown table generated in {output_file}")
+# Print the Markdown table to console
+print("::group::Cricket Players Table")
+print(markdown_table)
+print("::endgroup::")
